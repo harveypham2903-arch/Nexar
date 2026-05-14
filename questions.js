a16804c9231cdfc2acf794da606e5c3b6532d32c
harveypham2903-arch <harvey.pham2903@gmail.com>
2026-05-14 14:56:43 -0700

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,87 @@
+const questions = {
+
+  grade1: {
+
+    math: [
+      {
+        question: "2 + 2",
+        answer: "4"
+      },
+
+      {
+        question: "5 - 1",
+        answer: "4"
+      }
+    ],
+
+    english: [
+      {
+        question: "What letter comes after A?",
+        answer: "B"
+      }
+    ],
+
+    science: [
+      {
+        question: "What planet do we live on?",
+        answer: "Earth"
+      }
+    ]
+
+  },
+
+
+
+  grade2: {
+
+    math: [
+      {
+        question: "10 + 5",
+        answer: "15"
+      }
+    ],
+
+    english: [
+      {
+        question: "What is a noun?",
+        answer: "person place or thing"
+      }
+    ],
+
+    science: [
+      {
+        question: "What do plants need to grow?",
+        answer: "water"
+      }
+    ]
+
+  },
+
+
+
+  grade3: {
+
+    math: [
+      {
+        question: "12 x 2",
+        answer: "24"
+      }
+    ],
+
+    english: [
+      {
+        question: "What is the opposite of hot?",
+        answer: "cold"
+      }
+    ],
+
+    science: [
+      {
+        question: "What gas do humans breathe?",
+        answer: "oxygen"
+      }
+    ]
+
+  }
+
+};
